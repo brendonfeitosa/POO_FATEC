b53155e70e26cadd4c8ec548fe4a3ee0dd7758da
brendonfeitosa <brendonfeitosa@hotmail.com>
2023-05-19 22:17:50 -0300

    Aula 19/05/2023

---
+++ 25. InterfaceAutenticavel/Funcionario.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterfaceAutenticavel
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        
+    }
+}
---
+++ 25. InterfaceAutenticavel/Program.cs
@@ -0,0 +1,29 @@
+﻿// Função Main()
+
+using InterfaceAutenticavel;
+
+Gerente g = new Gerente();
+g.Nome = "Gerente";
+g.Senha = 123;
+
+Diretor d = new Diretor();
+d.Nome = "Diretor";
+d.Senha = 100;
+
+Cliente c = new Cliente();
+c.Nome = "Cliente";
+c.Endereco = "Rua xxxx, x";
+c.Senha = 200;
+
+//IAutenticavel ia = new IAutenticavel(); //vai dar erro
+
+IAutenticavel ia;
+
+ia = g;
+System.Console.WriteLine(g.Nome + " - " + ia.Autenticar(123));
+//ia.Autenticar(123);
+
+System.Console.WriteLine(d.Nome + " - " + ia.Autenticar(2));
+
+ia = c;
+System.Console.WriteLine(c.Nome + " - " + ia.Autenticar(200));
